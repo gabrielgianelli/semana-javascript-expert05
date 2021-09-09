9686ff3a01aab38a0ede24041f574eaf6c0bdcbb
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-09-09 01:30:12 -0300

    test: add test to write file stream

--- aulas/aula02/gdrive-webapi/test/_util/testUtil.js
+++ aulas/aula02/gdrive-webapi/test/_util/testUtil.js
@@ -1,4 +1,4 @@
-import { Readable } from 'stream';
+import { Readable, Transform, Writable } from 'stream';
 
 export default class TestUtil {
     
@@ -12,6 +12,30 @@ export default class TestUtil {
                 }
 
                 this.push(null);
+            }
+        });
+    }
+
+    static generateWritableStream(onData) {
+        return new Writable({
+            objectMode: true,
+            
+            write(chunk, encoding, cb) {
+                onData(chunk);
+                
+                cb(null, chunk);
+            }
+        });
+    }
+
+    static generateTransformStream(onData) {
+        return new Transform({
+            objectMode: true,
+
+            transform(chunk, encoding, cb) {
+                onData(chunk);
+
+                cb(null, chunk);
             }
         });
     }
--- aulas/aula02/gdrive-webapi/test/unit/uploadHandler.test.js
+++ aulas/aula02/gdrive-webapi/test/unit/uploadHandler.test.js
@@ -1,6 +1,8 @@
 import { describe, test, expect, jest } from '@jest/globals';
+// import { resolve } from 'path';
 import UploadHandler from '../../src/uploadHandler.js';
 import TestUtil from '../_util/testUtil.js';
+import fs from 'fs';
 
 describe('#UploadHandler test suite', () => {
     
@@ -36,6 +38,41 @@ describe('#UploadHandler test suite', () => {
             expect(onFinish).toHaveBeenCalled();
         });
 
+    });
+
+    describe('#onFile', () => {
+        test('given a stream file it should save it on disk', async () => {
+            const chunks = ['hey', 'dude'];
+            const downloadsFolder = '/tmp';
+            const handler = new UploadHandler({
+                io: ioObj,
+                socketId: '01',
+                downloadsFolder
+            });
+
+            const onData = jest.fn();
+            jest.spyOn(fs, fs.createWriteStream.name)
+                .mockImplementation(() => TestUtil.generateWritableStream(onData));
+
+            const onTransform = jest.fn();
+            jest.spyOn(handler, handler.handleFileBytes.name)            
+                .mockImplementation(() => TestUtil.generateTransformStream(onTransform));
+            
+            const params = {
+                fieldname: 'video',
+                file: TestUtil.generateReadableStream(chunks),
+                filename: 'mockfile.mov'
+            }
+
+            await handler.onFile(...Object.values(params));
+
+            expect(onData.mock.calls.join()).toEqual(chunks.join());
+            expect(onTransform.mock.calls.join()).toEqual(chunks.join());
+
+            // const expectedFilename = resolve(handler.downloadsFolder, params.filename);
+            const expectedFilename = `${handler.downloadsFolder}/${params.filename}`;
+            expect(fs.createWriteStream).toHaveBeenCalledWith(expectedFilename);
+        });
     });
 
 });
