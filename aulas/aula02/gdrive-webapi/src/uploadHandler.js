8b6fb8f0183f571f829915ec76639686a60dedd0
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-09-09 01:30:40 -0300

    feat: write file stream

--- aulas/aula02/gdrive-webapi/src/uploadHandler.js
+++ aulas/aula02/gdrive-webapi/src/uploadHandler.js
@@ -1,13 +1,33 @@
 import Busboy from 'busboy';
+import fs from 'fs';
+import { pipeline } from 'stream/promises';
+import { logger } from './logger.js';
 
 export default class UploadHandler {
     
-    constructor ({ io, socketId}) {
+    constructor ({ io, socketId, downloadsFolder }) {
+        this.io = io;
+        this.socketId = socketId;
+        this.downloadsFolder = downloadsFolder;
+    }
+
+    handleFileBytes() {
 
     }
 
-    onFile(fieldname, file, filename) {
+    async onFile(fieldname, file, filename) {
+        const saveTo = `${this.downloadsFolder}/${filename}`;
 
+        await pipeline(
+            // 1º passo, pegar uma readable stream!
+            file,
+            // 2º passo, filtrar, converter, transformar dados!
+            this.handleFileBytes.apply(this, [ filename ]),
+            // 3º passo, é a saída do processo, uma wirtable stream!
+            fs.createWriteStream(saveTo)
+        );
+
+        logger.info(`File [${filename}] finished`);
     }
 
     registerEvents(headers, onFinish) {
