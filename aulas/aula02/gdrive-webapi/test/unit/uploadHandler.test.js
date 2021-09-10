794a817ae4fdbef8a7a3ae0e16057c9907027ab9
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-09-09 22:44:53 -0300

    test: add test to transform file stream

--- aulas/aula02/gdrive-webapi/test/unit/uploadHandler.test.js
+++ aulas/aula02/gdrive-webapi/test/unit/uploadHandler.test.js
@@ -1,8 +1,10 @@
-import { describe, test, expect, jest } from '@jest/globals';
+import { describe, test, expect, beforeEach, jest } from '@jest/globals';
 // import { resolve } from 'path';
 import UploadHandler from '../../src/uploadHandler.js';
 import TestUtil from '../_util/testUtil.js';
+import { logger } from '../../src/logger.js';
 import fs from 'fs';
+import { pipeline } from 'stream/promises';
 
 describe('#UploadHandler test suite', () => {
     
@@ -10,6 +12,11 @@ describe('#UploadHandler test suite', () => {
         to: (id) => ioObj,
         emit: (event, message) => {}
     }
+
+    beforeEach(() => {
+        jest.spyOn(logger, 'info')
+            .mockImplementation();
+    });
 
     describe('#registerEvents', () => {
         
@@ -72,6 +79,38 @@ describe('#UploadHandler test suite', () => {
             // const expectedFilename = resolve(handler.downloadsFolder, params.filename);
             const expectedFilename = `${handler.downloadsFolder}/${params.filename}`;
             expect(fs.createWriteStream).toHaveBeenCalledWith(expectedFilename);
+        });
+    });
+
+    describe('#handleFileBytes', () => {
+        test('should call emit function and it is a transform stream', async () => {
+            jest.spyOn(ioObj, ioObj.to.name);
+            jest.spyOn(ioObj, ioObj.emit.name);
+
+            const handler = new UploadHandler({
+                io: ioObj,
+                socketId: '01'
+            });
+
+            const messages = ['hello'];
+            const source = TestUtil.generateReadableStream(messages);
+            const onWrite = jest.fn();
+            const target = TestUtil.generateWritableStream(onWrite);
+
+            await pipeline(
+                source,
+                handler.handleFileBytes('filename.txt'),
+                target
+            );
+
+            expect(ioObj.to).toHaveBeenCalledTimes(messages.length);
+            expect(ioObj.emit).toHaveBeenCalledTimes(messages.length);
+
+            // se o handleFileBytes for um transform string nosso pipeline 
+            // vai continuar o processo, passando os dados para frente
+            // e chamar nossa função no target a cada chunk
+            expect(ioObj.to).toHaveBeenCalledTimes(messages.length);
+            expect(onWrite.mock.calls.join()).toEqual(messages.join());
         });
     });
 
