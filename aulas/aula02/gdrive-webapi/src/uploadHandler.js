00e9155558413f13087f547dc5d0967aecc1cb68
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-09-09 23:39:30 -0300

    feat: manage back pressure

--- aulas/aula02/gdrive-webapi/src/uploadHandler.js
+++ aulas/aula02/gdrive-webapi/src/uploadHandler.js
@@ -5,21 +5,30 @@ import { logger } from './logger.js';
 
 export default class UploadHandler {
     
-    constructor ({ io, socketId, downloadsFolder }) {
+    constructor ({ io, socketId, downloadsFolder, messageTimeDelay = 200 }) {
         this.io = io;
         this.socketId = socketId;
         this.downloadsFolder = downloadsFolder;
         this.ON_UPLOAD_EVENT = 'file-ipload';
+        this.messageTimeDelay = messageTimeDelay;
+    }
+
+    canExecute(lastExecution) {
+        return (Date.now() - lastExecution) > this.messageTimeDelay;
     }
 
     handleFileBytes(filename) {
+        this.lastMessageSent = Date.now();
+
         async function* handleData(source) {
             let processedAlready = 0;
 
             for await(const chunk of source) {
                 yield chunk;
                 processedAlready += chunk.length;
-
+                if (!this.canExecute(this.lastMessageSent)) {
+                    continue;
+                }
                 this.io.to(this.socketId).emit(this.ON_UPLOAD_EVENT, {processedAlready, filename});
                 logger.info(`File [${filename}] got ${processedAlready} bytes to ${this.socketId}`);
             }
